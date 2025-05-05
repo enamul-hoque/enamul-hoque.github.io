dcf2555f3d487aaa52041c7c4f8afaf9af3c613b
Enamul Hoque Mohon <enamul.hoque.mohon@gmail.com>
2025-05-05 21:55:09 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,15 @@
 // Download Resume when ref=emran
-window.addEventListener('DOMContentLoaded', function () {
-    const params = new URLSearchParams(window.location.search);
+const params = new URLSearchParams(window.location.search);
 
-    if (params.get('ref') === 'emran') {
-        const link = document.createElement('a');
+if (params.get('ref') === 'emran') {
+    const link = document.createElement('a');
 
-        link.href = 'assets/pdf/Resume - Enamul Hoque Mohon.pdf';
-        // link.download = 'Resume - Enamul Hoque Mohon.pdf';
+    // link.href = 'assets/pdf/Resume - Enamul Hoque Mohon.pdf';
+    link.href = 'https://github.com/enamul-hoque';
+    // link.download = 'Resume - Enamul Hoque Mohon.pdf';
 
-        document.body.appendChild(link);
-        link.click();
-        
-        document.body.removeChild(link);
-    }
-});
+    document.body.appendChild(link);
+    link.click();
+    
+    document.body.removeChild(link);
+}
